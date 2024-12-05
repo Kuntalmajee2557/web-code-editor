aee1c3c64123e9d8df3ceffa71e382eea9846f7e
Kuntalmajee2557 <kuntalmajee338@gmail.com>
2024-12-06 03:23:45 +0530

    file stucture route created

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,11 @@ import express from "express"
 import { createServer } from "http";
 import { Server, Socket } from "socket.io";
 import * as nodePty from 'node-pty'
+import path from 'path'
 import os from "os"
+import fs from 'fs/promises'
+import cors from 'cors'
+
 
 const shell = process.env['/bin/sh'] as string;
 const ptyProcess = nodePty.spawn(shell, [], {
@@ -11,14 +15,15 @@ const ptyProcess = nodePty.spawn(shell, [], {
     rows: 30,
     cwd: process.env.INIT_CWD || process.cwd(),
     env: process.env
-  });
+});
 
 
 const app = express();
+app.use(cors())
 const server = createServer(app)
 const io = new Server(server, {
     cors: {
-        origin : "*"
+        origin: "*"
     }
 })
 
@@ -34,8 +39,35 @@ io.on('connection', (socket) => {
     })
 })
 
+app.get('/files', async (req, res) => {
+    const fileTree = await generateFiles("./user");
+    res.json({tree : fileTree});
+})
+
 
 server.listen(3000, () => {
     console.log("listening the port 3000")
-} )
+})
 
+
+async function buildingTree(currentDir: string, currentTree: { [key: string]: any}) {
+    const files = await fs.readdir(currentDir)
+
+    for (const file of files) {
+        const filePath = path.join(currentDir, file);
+        const stat = await fs.stat(filePath)
+        if (stat.isDirectory()){
+            currentTree[file] = {}
+            await buildingTree(filePath, currentTree[file])
+
+        } else {
+            currentTree[file] = null
+        }
+    }
+}
+
+async function generateFiles(directory: string) {
+    const tree = {}
+    await buildingTree(directory, tree);
+    return tree;
+}
