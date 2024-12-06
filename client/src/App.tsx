8ab0d0c7d1530eedb9a001f1255219a61f54d4c3
Kuntalmajee2557 <kuntalmajee338@gmail.com>
2024-12-06 21:10:51 +0530

    tree component stuctured

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,7 +17,7 @@ function App() {
   return (
     <div>
       <Terminal />
-      <Tree fileObj={fileTree} />
+      <Tree tree={fileTree} />
     </div>
   )
 }
--- client/src/components/Tree.tsx
+++ client/src/components/Tree.tsx
@@ -1,23 +1,36 @@
-import React, { useEffect, useState } from 'react'
+//use react-folder-tree for ui
 import "./Tree.css"
 
-function Tree({ fileObj }: { fileObj: { [key: string]: any } }) {
-    return (
-        <ul>
-            {Object.keys(fileObj as Object).map((child) => {
-                if (fileObj[child] == null) {
-                    return <li key={child}>{child}</li>
-                }
-                else {
-                    return (
-                        <li key={child}>{child}
-                            <Tree fileObj={fileObj[child]} /></li>
-                    )
+interface FileTreeNodeProps {
+    fileName: string;
+    nodes: { [key: string]: any } | any
+}
 
-                }
-            })}
-        </ul>
+function FileTreeNode({ fileName, nodes }: FileTreeNodeProps) {
+    const isDir: boolean = !!nodes;
+    return (
+        <div style={{ "paddingLeft": "20px" }}>
+            <p className={isDir ? "" : "file-node "}>{fileName}</p>
+            {nodes && (
+                <ul>
+                    {Object.keys(nodes as Object).map((child) =>
+
+                    (
+                        <li key={child}>
+                            <FileTreeNode fileName={child} nodes={nodes[child]} /></li>
+                    )
+                    )}
+                </ul>
+            )}
+        </div>
+    )
+}
+
+function Tree({ tree }: { tree: { [key: string]: any } }) {
+    return (
+        <FileTreeNode fileName='/' nodes={tree} />
     )
 }
 
 export default Tree
+
